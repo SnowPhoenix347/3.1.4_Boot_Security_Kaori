03601a57e63cc8f0218efc78a518b36c8e63b847
snowphoenix347 <snowphoenix347@gmail.com>
2024-10-09 12:43:19 +0300

    Фиксы чтения ролей и верстки

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -18,7 +18,7 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_ADMIN")) {
-            httpServletResponse.sendRedirect("/admins/admin");
+            httpServletResponse.sendRedirect("/admins");
         } else if (roles.contains("ROLE_USER")) {
             httpServletResponse.sendRedirect("/user");
         } else {
